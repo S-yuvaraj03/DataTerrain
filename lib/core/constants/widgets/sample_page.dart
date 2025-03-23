89f78df47dbb9bb4181c94e0711d1fa9a47b27a6
S-yuvaraj03 <152088227+S-yuvaraj03@users.noreply.github.com>
2025-03-23 23:54:14 +0530

    commiting final updates

--- lib/core/constants/widgets/sample_page.dart
+++ lib/core/constants/widgets/sample_page.dart
@@ -1,8 +1,9 @@
 import 'package:dataterrain/core/constants/app_assets.dart';
+import 'package:dataterrain/widgets/appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../widgets/drawer.dart';
 import '../app_text.dart';
 
 class UnderMaintenance extends StatelessWidget {
@@ -11,6 +12,9 @@ class UnderMaintenance extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: const KDrawer(),
+      backgroundColor: Colors.white70,
+      appBar: const KAppBar(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
